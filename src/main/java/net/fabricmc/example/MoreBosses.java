42fd259b60278d3d05d7175b3446abcf0d031975
Deven Mallamo <neved1234@gmail.com>
2022-05-15 21:11:09 -0400

    created zombie_scarecrow block with basic texture

--- src/main/java/net/fabricmc/example/MoreBosses.java
+++ src/main/java/net/fabricmc/example/MoreBosses.java
@@ -1,6 +1,14 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.example.blocks.ZombieScarecrow;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +18,9 @@ public class MoreBosses implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("MoreMobs");
 
+	public static final ZombieScarecrow ZOMBIE_SCARECROW = new ZombieScarecrow(
+			FabricBlockSettings.of(Material.STONE).hardness(4.0f));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -17,5 +28,9 @@ public class MoreBosses implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.BLOCK, new Identifier(
+				"more_bosses", "zombie_scarecrow"), ZOMBIE_SCARECROW);
+		Registry.register(Registry.ITEM, new Identifier("more_bosses", "zombie_scarecrow"),
+				new BlockItem(ZOMBIE_SCARECROW, new Item.Settings().group(ItemGroup.MISC)));
 	}
 }
---
+++ src/main/java/net/fabricmc/example/blocks/ZombieScarecrow.java
@@ -0,0 +1,11 @@
+package net.fabricmc.example.blocks;
+
+import net.minecraft.block.Block;
+
+public class ZombieScarecrow extends Block{
+    public ZombieScarecrow(Block.Settings settings) {
+        super(settings);
+    }
+
+
+}
