87e0d647ae596d8c0dbb7a06c5f83be2409a7126
Deven Mallamo <neved1234@gmail.com>
2022-05-31 10:07:34 -0400

    improved behavior for tower of undying

--- src/main/java/net/fabricmc/example/items/TowerOfUndying.java
+++ src/main/java/net/fabricmc/example/items/TowerOfUndying.java
@@ -1,9 +1,13 @@
 package net.fabricmc.example.items;
 
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.raid.RaiderEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.village.raid.Raid;
@@ -20,7 +24,17 @@ public class TowerOfUndying extends Item {
         if(!world.isClient()){
             ServerWorld serverWorld = (ServerWorld)world;
             Raid raid = serverWorld.getRaidAt(user.getBlockPos());
-            raid.invalidate();
+            if(raid.isActive()){
+                raid.invalidate();
+                if(user.isCreative()){
+                    itemStack.decrement(1);
+                }
+            }
+            for(RaiderEntity raider : raid.getAllRaiders()){
+                raider.kill();
+            }
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.HERO_OF_THE_VILLAGE, 48000, 1, false, false, true));
+            user.incrementStat(Stats.RAID_WIN);
             return TypedActionResult.success(itemStack, true);
         }
 
