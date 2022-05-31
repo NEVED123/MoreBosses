b86bba508a441883eed1d5072eaa4128d8d2cac2
Deven Mallamo <neved1234@gmail.com>
2022-05-31 09:40:28 -0400

    tower of undying invalidates raid

--- src/main/java/net/fabricmc/example/items/TowerOfUndying.java
+++ src/main/java/net/fabricmc/example/items/TowerOfUndying.java
@@ -1,6 +1,13 @@
 package net.fabricmc.example.items;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.village.raid.Raid;
+import net.minecraft.world.World;
 
 public class TowerOfUndying extends Item {
 
@@ -8,5 +15,17 @@ public class TowerOfUndying extends Item {
         super(settings);
     }
 
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand){
+        ItemStack itemStack = user.getStackInHand(hand);
+        if(!world.isClient()){
+            ServerWorld serverWorld = (ServerWorld)world;
+            Raid raid = serverWorld.getRaidAt(user.getBlockPos());
+            raid.invalidate();
+            return TypedActionResult.success(itemStack, true);
+        }
+
+        return TypedActionResult.fail(itemStack);
+
+    }
 
 }
