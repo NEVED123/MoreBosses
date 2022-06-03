b9094dc0eaa2e2a5257f9321437d0604313cb228
NEVED123 <neved1234@gmail.com>
2022-06-03 16:22:50 -0400

    fixed tower of undying always giving hero of village effect + exceptions, when item is used it spawns firework

--- src/main/java/net/fabricmc/example/entities/TowerOfUndyingEntity.java
+++ src/main/java/net/fabricmc/example/entities/TowerOfUndyingEntity.java
@@ -1,5 +1,6 @@
 package net.fabricmc.example.entities;
 
+import com.mojang.authlib.yggdrasil.response.User;
 import net.minecraft.entity.*;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -13,16 +14,19 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 
-public class TowerOfUndyingEntity extends FireworkRocketEntity {
+public class TowerOfUndyingEntity extends FireworkRocketEntity{
 
     public TowerOfUndyingEntity(EntityType<? extends FireworkRocketEntity> entityType, World world) {
         super(entityType, world);
     }
 
-
+    public TowerOfUndyingEntity(World world, @Nullable Entity entity, double x, double y, double z, ItemStack stack){
+        super(world, entity, x, y, z, stack);
+    }
 
 
 
--- src/main/java/net/fabricmc/example/items/TowerOfUndying.java
+++ src/main/java/net/fabricmc/example/items/TowerOfUndying.java
@@ -1,15 +1,20 @@
 package net.fabricmc.example.items;
 
+import net.fabricmc.example.entities.TowerOfUndyingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.entity.raid.RaiderEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.village.raid.Raid;
 import net.minecraft.world.World;
 
@@ -20,21 +25,21 @@ public class TowerOfUndying extends Item {
     }
 
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand){
+
         ItemStack itemStack = user.getStackInHand(hand);
         if(!world.isClient()){
+            TowerOfUndyingEntity towerOfUndyingEntity = new TowerOfUndyingEntity(world, null, user.getX(), user.getY(), user.getZ(), itemStack);
+            world.spawnEntity(towerOfUndyingEntity);
             ServerWorld serverWorld = (ServerWorld)world;
             Raid raid = serverWorld.getRaidAt(user.getBlockPos());
-            if(raid.isActive()){
+            if(raid != null && raid.isActive()){
                 raid.invalidate();
-                if(user.isCreative()){
-                    itemStack.decrement(1);
+                for(RaiderEntity raider : raid.getAllRaiders()){
+                    raider.kill();
                 }
+                user.addStatusEffect(new StatusEffectInstance(StatusEffects.HERO_OF_THE_VILLAGE, 48000, 1, false, false, true));
+                user.incrementStat(Stats.RAID_WIN);
             }
-            for(RaiderEntity raider : raid.getAllRaiders()){
-                raider.kill();
-            }
-            user.addStatusEffect(new StatusEffectInstance(StatusEffects.HERO_OF_THE_VILLAGE, 48000, 1, false, false, true));
-            user.incrementStat(Stats.RAID_WIN);
             return TypedActionResult.success(itemStack, true);
         }
 
