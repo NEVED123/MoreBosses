54de3a4393040749c11f9a4d31aebb64e5b82864
NEVED123 <neved1234@gmail.com>
2022-06-11 11:01:06 -0400

    added rest of wings

--- src/main/java/net/fabricmc/example/models/TowerOfUndyingEntityModel.java
+++ src/main/java/net/fabricmc/example/models/TowerOfUndyingEntityModel.java
@@ -21,7 +21,11 @@ public class TowerOfUndyingEntityModel extends SinglePartEntityModel<TowerOfUndy
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
         modelPartData.addChild("base", ModelPartBuilder.create().uv(0, 0).cuboid(-2F, 8F, -2F, 4F, 16F, 4F), ModelTransform.pivot(0F, 0F, 0F));
-        modelPartData.addChild("top_wing", ModelPartBuilder.create().uv(0,20).cuboid(-1F, 12F, 2F, 2F, 2F, 4F), ModelTransform.pivot(0F,0F,0F));
+        modelPartData.addChild("right_top_wing", ModelPartBuilder.create().uv(0,20).cuboid(-1F, 12F, 2F, 2F, 2F, 8F), ModelTransform.pivot(0F,0F,0F));
+        modelPartData.addChild("left_top_wing", ModelPartBuilder.create().uv(0,20).mirrored().cuboid(-1F,12F, -10F, 2F, 2F, 8F), ModelTransform.pivot(0F, 0F,0F));
+        modelPartData.addChild("right_bottom_wing", ModelPartBuilder.create().uv(0, 30).cuboid(-1F, 16F, 2F, 2F, 2F, 6F), ModelTransform.pivot(0F,0F,0F));
+        modelPartData.addChild("left_bottom_wing", ModelPartBuilder.create().uv(0,30).mirrored().cuboid(-1F,16F, -8F, 2F, 2F, 6F), ModelTransform.pivot(0F, 0F,0F));
+
         return TexturedModelData.of(modelData, 64, 64);
     }
 
