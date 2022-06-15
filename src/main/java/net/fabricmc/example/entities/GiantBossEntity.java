48dbf4bf7fddfd4c7ee97e0654e862ee00f23130
NEVED123 <neved1234@gmail.com>
2022-06-14 21:02:18 -0400

    giant boss attacks player in the most fucking terrifying way possible

--- src/main/java/net/fabricmc/example/entities/GiantBossEntity.java
+++ src/main/java/net/fabricmc/example/entities/GiantBossEntity.java
@@ -1,15 +1,22 @@
 package net.fabricmc.example.entities;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.ActiveTargetGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.mob.GiantEntity;
+import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 public class GiantBossEntity extends GiantEntity {
     public GiantBossEntity(EntityType<? extends GiantEntity> entityType, World world) {
         super(entityType, world);
+    }
 
+    protected void initGoals(){
+        this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+        this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
+        this.goalSelector.add(8, new MeleeAttackGoal(this, 2.0, true));
     }
 
 }
