771954b4f2a84f05d4e01b162d89192be7e6ae08
NEVED123 <neved1234@gmail.com>
2022-06-18 12:56:16 -0400

    adjusted attack speed

--- src/main/java/net/fabricmc/example/entities/GiantBossEntity.java
+++ src/main/java/net/fabricmc/example/entities/GiantBossEntity.java
@@ -2,6 +2,7 @@ package net.fabricmc.example.entities;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.GiantEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,7 +17,9 @@ public class GiantBossEntity extends GiantEntity {
         this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
         this.goalSelector.add(8, new LookAroundGoal(this));
         this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
-        this.goalSelector.add(8, new MeleeAttackGoal(this, 2.0, true));
+        this.goalSelector.add(8, new MeleeAttackGoal(this, 1.0, true));
     }
+
+
 
 }
