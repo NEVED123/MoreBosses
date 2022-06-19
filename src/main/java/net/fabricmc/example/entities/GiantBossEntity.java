a5e4ade55f0befa0cdfad659d9117bd0a2175a94
NEVED123 <neved1234@gmail.com>
2022-06-19 15:39:32 -0400

    added bossbar, fixed hitbox issue by adding attributes to attribute builder

--- src/main/java/net/fabricmc/example/entities/GiantBossEntity.java
+++ src/main/java/net/fabricmc/example/entities/GiantBossEntity.java
@@ -5,16 +5,23 @@ import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.boss.BossBar;
+import net.minecraft.entity.boss.ServerBossBar;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.GiantEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
+import org.lwjgl.system.Pointer;
 
 public class GiantBossEntity extends GiantEntity {
+
+    private ServerBossBar bossBar;
     public GiantBossEntity(EntityType<? extends GiantEntity> entityType, World world) {
         super(entityType, world);
+        bossBar = (ServerBossBar)(new ServerBossBar(this.getDisplayName(), BossBar.Color.PURPLE, BossBar.Style.PROGRESS)).setDarkenSky(true);
     }
 
     protected void initGoals(){
@@ -32,7 +39,14 @@ public class GiantBossEntity extends GiantEntity {
                 .add(EntityAttributes.GENERIC_ATTACK_SPEED, 2)
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 5)
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 200)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1);
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1)
+                .add(EntityAttributes.GENERIC_ARMOR)
+                .add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS);
+    }
+
+    public void onStartedTrackingBy(ServerPlayerEntity player){
+        super.onStartedTrackingBy(player);
+        bossBar.addPlayer(player);
     }
 
 }
