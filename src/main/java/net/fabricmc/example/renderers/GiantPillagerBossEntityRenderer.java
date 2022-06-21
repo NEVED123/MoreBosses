35a4e27e7b845affec2a7a51139af8ecd540c4b1
NEVED123 <neved1234@gmail.com>
2022-06-21 19:16:39 -0400

    giant pillager has a janky yet new texture

--- src/main/java/net/fabricmc/example/renderers/GiantPillagerBossEntityRenderer.java
+++ src/main/java/net/fabricmc/example/renderers/GiantPillagerBossEntityRenderer.java
@@ -1,10 +1,17 @@
 package net.fabricmc.example.renderers;
 
+import net.fabricmc.example.entities.GiantPillagerBossEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.GiantEntityRenderer;
+import net.minecraft.entity.mob.GiantEntity;
+import net.minecraft.util.Identifier;
 
 public class GiantPillagerBossEntityRenderer extends GiantEntityRenderer {
     public GiantPillagerBossEntityRenderer(EntityRendererFactory.Context ctx) {
         super(ctx, 6);
+    }
+
+    public Identifier getTexture(GiantEntity entity){
+        return new Identifier("more_bosses", "textures/entity/giant_pillager_boss/giant_pillager_boss.png");
     }
 }
