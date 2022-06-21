74669d3bcee12c42474d90023475c8f82259e613
NEVED123 <neved1234@gmail.com>
2022-06-21 14:14:22 -0400

    added similar behavior to giant zombie boss

--- src/main/java/net/fabricmc/example/MoreBosses.java
+++ src/main/java/net/fabricmc/example/MoreBosses.java
@@ -56,7 +56,7 @@ public class MoreBosses implements ModInitializer {
 		FabricDefaultAttributeRegistry.register(ZOMBIE_SCARECROW_ENTITY, ZombieScarecrowEntity.createLivingAttributes());
 		FabricDefaultAttributeRegistry.register(TOWER_OF_UNDYING_ENTITY, TowerOfUndyingEntity.createLivingAttributes());
 		FabricDefaultAttributeRegistry.register(GIANT_BOSS_ENTITY, GiantBossEntity.createGiantEntityAttributes());
-		FabricDefaultAttributeRegistry.register(GIANT_PILLAGER_BOSS_ENTITY, GiantPillagerBossEntity.createHostileAttributes());
+		FabricDefaultAttributeRegistry.register(GIANT_PILLAGER_BOSS_ENTITY, GiantPillagerBossEntity.createGiantPillagerEntityAttributes());
 
 		Registry.register(Registry.ITEM, new Identifier("more_bosses", "zombie_scarecrow_item"), ZOMBIE_SCARECROW_ITEM);
 		Registry.register(Registry.ITEM, new Identifier("more_bosses", "tower_of_undying_item"), TOWER_OF_UNDYING_ITEM);
