5c87ba97453be98451cb8500ea39dbc1f7355693
NEVED123 <neved1234@gmail.com>
2022-06-26 10:39:04 -0400

    pillager boss now spawns occasional ravager as reinforcement, created private Enum for list of reinforcements

--- src/main/java/net/fabricmc/more_bosses/entities/bosses/GiantPillagerBossEntity.java
+++ src/main/java/net/fabricmc/more_bosses/entities/bosses/GiantPillagerBossEntity.java
@@ -4,6 +4,7 @@ import net.fabricmc.more_bosses.entities.bosses.GiantBossEntity;
 import net.minecraft.entity.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.RavagerEntity;
 import net.minecraft.entity.mob.VexEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -39,11 +40,18 @@ public class GiantPillagerBossEntity extends GiantBossEntity{
             Vec3d playerPos = this.getTarget().getPos();
             Vec3d bossPos = this.getPos();
             float playerDistance = distance(playerPos, bossPos);
-            if(playerDistance < 15 && !world.isClient() && random.nextFloat() < .1){
+            if(playerDistance < 15 && !world.isClient()){
                 ServerWorld serverWorld = (ServerWorld)world;
-                BlockPos spawnLocation = reinforcementSpawnLocation(this.getBlockPos(), 10);
-                VexEntity vex = EntityType.VEX.create(serverWorld, null, null, null, spawnLocation, SpawnReason.REINFORCEMENT, true, false);
-                serverWorld.spawnEntity(vex);
+                if(random.nextFloat() < .1){
+                    BlockPos spawnLocation = reinforcementSpawnLocation(this.getBlockPos(), 10, Reinforcement.VEX);
+                    VexEntity vex = EntityType.VEX.create(serverWorld, null, null, null, spawnLocation, SpawnReason.REINFORCEMENT, true, false);
+                    serverWorld.spawnEntity(vex);
+                }
+                if(random.nextFloat() < .002){
+                    BlockPos spawnLocation = reinforcementSpawnLocation(this.getBlockPos(), 10, Reinforcement.RAVAGER);
+                    RavagerEntity ravager = EntityType.RAVAGER.create(serverWorld, null, null, null, spawnLocation, SpawnReason.REINFORCEMENT, true, false);
+                    serverWorld.spawnEntity(ravager);
+                }
             }
         }
     }
@@ -52,14 +60,28 @@ public class GiantPillagerBossEntity extends GiantBossEntity{
         return (float)Math.sqrt(Math.pow(i.x - j.x, 2) + Math.pow(i.y - j.y, 2) + Math.pow(i.z - j.z, 2));
     }
 
-    private BlockPos reinforcementSpawnLocation(BlockPos source, float range){
+    private BlockPos reinforcementSpawnLocation(BlockPos source, float range, Reinforcement mob){
         Random spawnDistance = new Random();
         Random isNegative = new Random();
-        float spawnX = isNegative.nextBoolean() ? source.getX() + spawnDistance.nextFloat() * -range : source.getX() + spawnDistance.nextFloat() * range;
-        float spawnY = source.getY() + spawnDistance.nextFloat() * range;
-        float spawnZ = isNegative.nextBoolean() ? source.getZ() + spawnDistance.nextFloat() * -range : source.getZ() + spawnDistance.nextFloat() * range;
-        System.out.println(spawnX + "," + spawnY + "," + spawnZ);
+        float spawnX = 0;
+        float spawnY = 0;
+        float spawnZ = 0;
+        if(mob.equals(Reinforcement.VEX)){
+            spawnX = isNegative.nextBoolean() ? source.getX() + spawnDistance.nextFloat() * -range : source.getX() + spawnDistance.nextFloat() * range;
+            spawnY = source.getY() + spawnDistance.nextFloat() * range;
+            spawnZ = isNegative.nextBoolean() ? source.getZ() + spawnDistance.nextFloat() * -range : source.getZ() + spawnDistance.nextFloat() * range;
+        }
+        if(mob.equals(Reinforcement.RAVAGER)){
+            spawnX = isNegative.nextBoolean() ? source.getX() + spawnDistance.nextFloat() * -range : source.getX() + spawnDistance.nextFloat() * range;
+            spawnY = source.getY();
+            spawnZ = isNegative.nextBoolean() ? source.getZ() + spawnDistance.nextFloat() * -range : source.getZ() + spawnDistance.nextFloat() * range;
+        }
+
         return new BlockPos(spawnX, spawnY, spawnZ);
+    }
+
+    private enum Reinforcement{
+        VEX, RAVAGER
     }
 
 
