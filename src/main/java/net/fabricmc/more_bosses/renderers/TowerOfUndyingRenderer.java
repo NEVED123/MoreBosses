958b2ffcaa5bc11d6b9e53c4e1a8a3158755f9ad
NEVED123 <neved1234@gmail.com>
2022-07-01 17:47:12 -0400

    finally fixed label rendering issue omg that sucked

--- src/main/java/net/fabricmc/more_bosses/renderers/TowerOfUndyingRenderer.java
+++ src/main/java/net/fabricmc/more_bosses/renderers/TowerOfUndyingRenderer.java
@@ -3,8 +3,11 @@ package net.fabricmc.more_bosses.renderers;
 import net.fabricmc.more_bosses.MoreBossesClient;
 import net.fabricmc.more_bosses.entities.TowerOfUndyingEntity;
 import net.fabricmc.more_bosses.models.TowerOfUndyingEntityModel;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class TowerOfUndyingRenderer extends LivingEntityRenderer<TowerOfUndyingEntity, TowerOfUndyingEntityModel> {
@@ -13,10 +16,13 @@ public class TowerOfUndyingRenderer extends LivingEntityRenderer<TowerOfUndyingE
         super(context, new TowerOfUndyingEntityModel(context.getPart(MoreBossesClient.MODEL_TOWER_OF_UNDYING_LAYER)), 0.5f);
 
     }
-
     @Override
     public Identifier getTexture(TowerOfUndyingEntity entity) {
         return new Identifier("more_bosses", "textures/entity/tower_of_undying/tower_of_undying.png");
+    }
+
+    protected void renderLabelIfPresent(TowerOfUndyingEntity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light){
+
     }
 
 }
--- src/main/java/net/fabricmc/more_bosses/renderers/ZombieScarecrowRenderer.java
+++ src/main/java/net/fabricmc/more_bosses/renderers/ZombieScarecrowRenderer.java
@@ -1,11 +1,15 @@
 package net.fabricmc.more_bosses.renderers;
 
+import net.fabricmc.more_bosses.entities.TowerOfUndyingEntity;
 import net.fabricmc.more_bosses.entities.ZombieScarecrowEntity;
 import net.fabricmc.more_bosses.MoreBossesClient;
 import net.fabricmc.more_bosses.models.ZombieScarecrowEntityModel;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ZombieScarecrowRenderer extends LivingEntityRenderer<ZombieScarecrowEntity, BipedEntityModel<ZombieScarecrowEntity>> {
@@ -16,6 +20,12 @@ public class ZombieScarecrowRenderer extends LivingEntityRenderer<ZombieScarecro
     @Override
     public Identifier getTexture(ZombieScarecrowEntity entity) {
         return new Identifier("more_bosses", "textures/entity/zombie_scarecrow/zombie_scarecrow.png");
+    }
+
+    protected void renderLabelIfPresent(ZombieScarecrowEntity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light){
+        if(entity.hasCustomName()){
+            super.renderLabelIfPresent(entity, text, matrices, vertexConsumers, light);
+        }
     }
 
 
